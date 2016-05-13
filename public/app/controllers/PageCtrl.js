57a780369977c376176945e088584c32a9882dbd
Pedro Fernandes <pedro_fernandes@live.com>
2016-05-13 15:14:30 +0100

    ...

--- public/app/controllers/PageCtrl.js
+++ public/app/controllers/PageCtrl.js
@@ -6,10 +6,16 @@
 	 var  PageCtrl = function($scope, $routeParams, $window, services) {
 
 		 console.log('Page loaded.');
+		 
+		 $scope.loggedIn = function() {
+			return true;
+		}
 
 	 };
 	 // Injecting modules used for better minifing later on
     PageCtrl.$inject = ['$scope', 'services'];
+	
+	
 
     // Enabling the controller in the app
     angular.module('lessonslearned').controller('PageCtrl', PageCtrl);
