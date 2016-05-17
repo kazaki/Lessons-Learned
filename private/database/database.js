be04937377011084e2c653ae61a98b9ff255f7e2
Sara Silva <ei11096@fe.up.pt>
2016-05-17 16:45:25 +0100

    Weird, stupid bug

--- private/database/database.js
+++ private/database/database.js
@@ -77,7 +77,7 @@
          client.query(query,function (err, result) {
                     if (err) {
                         reject(err);
-                    } else if(result!=[]){
+                    } else if(result!=[] && result.length > 0){
                         delete result[0]['password'];
                         delete result[0]['idusers'];
                         resolve(result[0]);
