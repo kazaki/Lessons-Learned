d8016b047e15e570c7b68a5a511d1518715ea6ff
Pedro Fernandes <pedro_fernandes@live.com>
2016-05-19 19:59:01 +0100

    fixes

--- public/app/controllers/ViewLLCtrl.js
+++ public/app/controllers/ViewLLCtrl.js
@@ -11,6 +11,11 @@
 				.then(function (res) {
 				
 				var data = res.data[0];
+				
+				if (data == null) {
+					console.log("Invalid LL id.")
+					return;
+				} 
 
 				console.log(data);
 				$("#vll_title").val(data["name"]);
@@ -21,6 +26,20 @@
 			});
 			
 		}
+		
+		$scope.loadLL = function() {
+			var parameters = location.search.substring(1).split("&");
+			var args = [];
+			for (var i=0; i < parameters.length; i++) {
+				 var temp = parameters[i].split("=");
+				 args[temp[0]] = temp[1];
+			}
+			if (args["id"] != null) {
+				$scope.getLesson(args["id"]);
+			}
+		}
+		
+		$scope.loadLL();
 
 	 };
 	 // Injecting modules used for better minifing later on
