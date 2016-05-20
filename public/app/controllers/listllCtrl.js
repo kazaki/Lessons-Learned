0ea3904e9b1f68efbb3374ab948682a802f2b1cb
vania-leite <ei12037@fe.up.pt>
2016-05-20 18:39:01 +0100

    showing online and offline lessons for admin

--- public/app/controllers/listllCtrl.js
+++ public/app/controllers/listllCtrl.js
@@ -5,6 +5,8 @@
     var listllCtrl = function($scope, listllServices, userServices, filterFilter, $filter) {
         $scope.isAdmin = 0;
         $scope.sortType = 'title';
+        $scope.statusSel = false;
+        $scope.statusString = "Draft";
         console.log('Page loaded.');
 
         userServices.logged()
@@ -40,10 +42,53 @@
                 alert(err.data.message);
             });
 
+        $scope.clickStatus = function() {
+            $scope.sortType = 'status';
+            $scope.StatusSel = !$scope.StatusSel
+            console.log($scope.StatusSel);
+            if ($scope.StatusSel == true) {
+                $scope.statusString = "Online";
+            } else {
+                $scope.statusString = "Offline";
+            }
+            console.log($scope.statusString);
+        }
+
+        $scope.OnlineOrOffline = function(status) {
+            return function(lesson) {
+                console.log("NO SERV:" + $scope.statusString);
+                if ($scope.statusString == "Offline") {
+                    return lesson.status == 'Draft' || lesson.status == 'Inactive';
+                } else {
+                    return lesson.status == 'Active';
+                }
+            }
+
+        };
+
 
 
 
     };
+
+    /*
+    app.filter('filterStatus', function() {
+        return function(lessons, StatusSel) {
+            if (StatusSel == 1) {
+                lessons = $filter('filter')(result.data, {
+                    status: "Active"
+                }, true);
+            }
+            else{
+              lessons = $filter('filter')(result.data, {
+                  status: "Inactive"
+              }, true);
+            }
+        };
+
+    });
+
+    */
     // Injecting modules used for better minifing later on
     listllCtrl.$inject = ['$scope', 'listllServices', 'userServices', 'filterFilter', '$filter'];
 
